31f26f70c9df1145a3c4fdcb4319789dded5b7d6
ABHINAY2025 <158329023+ABHINAY2025@users.noreply.github.com>
2025-11-14 15:01:48 +0530

    Fix Vercel Python handler

--- api/index.py
+++ api/index.py
@@ -1,4 +1,14 @@
-from app import app as application  # import your Flask app
+from app import app
+from werkzeug.middleware.dispatcher import DispatcherMiddleware
+from werkzeug.wrappers import Request, Response
+
+# Convert Flask app to WSGI application
+wsgi_app = DispatcherMiddleware(app)
 
 def handler(request, response):
-    return application(request, response)
+    req = Request(request.environ)
+    res = Response.from_app(wsgi_app, req.environ)
+    response.status_code = res.status_code
+    for k, v in res.headers:
+        response.headers[k] = v
+    response.set_body(res.get_data())
